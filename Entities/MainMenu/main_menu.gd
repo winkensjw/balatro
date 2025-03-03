65d86c4aa5c0af2e9fadb7dfdeacf843d3a2d1fe
winkensjw <winkensjw@gmail.com>
2025-03-03 17:50:21 +0100

    WIP Added Main Menu

--- Entities/MainMenu/main_menu.gd
+++ Entities/MainMenu/main_menu.gd
@@ -1,1 +1,11 @@
 extends CanvasLayer
+
+
+func _on_profile_button_pressed() -> void:
+	Log.info("Profile button pressed")
+	pass # Replace with function body.
+
+
+func _on_language_button_pressed() -> void:
+	Log.info("Language button pressed")
+	pass # Replace with function body.
